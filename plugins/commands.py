30961e864ebec75a5e066e1f1d22683973a96217
Hansaka Anuhas <121796740+Hansaka-Anuhas@users.noreply.github.com>
2023-02-02 13:47:07 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -44,8 +44,13 @@ async def start(client, message):
         )
         return
     if AUTH_CHANNEL and not await is_subscribed(client, message):
+        try:
+            invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
+        except ChatAdminRequired:
+            logger.error("Make sure Bot is admin in Forcesub channel")
+            return
         btn = [[
-            InlineKeyboardButton("📢 Updates Channel 📢", url='https://t.me/SL_Auto_Filter_Bot_Updates')
+            InlineKeyboardButton("📢 Updates Channel 📢", url=invite_link.invite_link)
         ]]
 
         if message.command[1] != "subscribe":
