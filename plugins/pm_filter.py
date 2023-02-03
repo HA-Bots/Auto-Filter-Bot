cbb82f3bad5ebd2ac9cefb7fbdcf89624c9894da
Hansaka Anuhas <121796740+Hansaka-Anuhas@users.noreply.github.com>
2023-02-03 10:35:32 +0530

    modified spell check!

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -398,6 +398,9 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
     elif query.data == "buttons":
         await query.answer()
+        
+    elif query.data == "instructions":
+        await query.answer("Movie request format.\n\nExample:\nBlack Adam or Black Adam 2022\n\nReries request format.\nLoki S01E01 or Loki S01 E01\n\nDon't use symbols.)
 
     elif query.data == "start":
         await query.answer('Welcome!')
@@ -790,20 +793,76 @@ async def auto_filter(client, msg, spoll=False):
 async def advantage_spell_chok(msg):
     search = msg.text
     search_google = search.replace(" ", "+")
-    user = msg.from_user.id if msg.from_user else 0
-    search_imdb = await get_poster(search, bulk=True)
-    if search_imdb:
-        movie_list = [movie.get('title') for movie in search_imdb]
-        if not movie_list:
-            await msg.reply("I couldn't find anything related to that. Check your spelling")
-            return
-        SPELL_CHECK[msg.id] = movie_list
+    query = re.sub(
+        r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)",
+        "", msg.text, flags=re.IGNORECASE)  # plis contribute some common words
+    query = query.strip() + " movie"
+    g_s = await search_gagala(query)
+    g_s += await search_gagala(msg.text)
+    gs_parsed = []
+    if not g_s:
         btn = [[
-            InlineKeyboardButton(
-                text=movie.strip(),
-                callback_data=f"spolling#{user}#{k}",
-            )
-            ] for k, movie in enumerate(movie_list)]
-        await msg.reply("I couldn't find anything related to that\nDid you mean any one of these?",
+            InlineKeyboardButton("⚠️ Instructions ⚠️", callback_data='instructions'),
+            InlineKeyboardButton("🔎 Search Google 🔍", url=f"https://www.google.com/search?q={search_google}")
+        ]]
+        k = await msg.reply_photo(photo=random.choice(PICS), caption=f"👋 Hello {msg.from_user.mention},\n\nI can't find <b>'{search}'</b> you requested. 😢</b>", reply_markup=InlineKeyboardMarkup(btn))
+        await asyncio.sleep(60)
+        await k.delete()
+        try:
+            await msg.delete()
+        except:
+            pass
+        return
+    regex = re.compile(r".*(imdb|wikipedia).*", re.IGNORECASE)  # look for imdb / wiki results
+    gs = list(filter(regex.match, g_s))
+    gs_parsed = [re.sub(
+        r'\b(\-([a-zA-Z-\s])\-\simdb|(\-\s)?imdb|(\-\s)?wikipedia|\(|\)|\-|reviews|full|all|episode(s)?|film|movie|series)',
+        '', i, flags=re.IGNORECASE) for i in gs]
+    if not gs_parsed:
+        reg = re.compile(r"watch(\s[a-zA-Z0-9_\s\-\(\)]*)*\|.*",
+                         re.IGNORECASE)  # match something like Watch Niram | Amazon Prime
+        for mv in g_s:
+            match = reg.match(mv)
+            if match:
+                gs_parsed.append(match.group(1))
+    user = msg.from_user.id if msg.from_user else 0
+    movielist = []
+    gs_parsed = list(dict.fromkeys(gs_parsed))  # removing duplicates
+    if len(gs_parsed) > 3:
+        gs_parsed = gs_parsed[:3]
+    if gs_parsed:
+        for mov in gs_parsed:
+            imdb_s = await get_poster(mov.strip(), bulk=True)  # searching each keyword in imdb
+            if imdb_s:
+                movielist += [movie.get('title') for movie in imdb_s]
+    movielist += [(re.sub(r'(\-|\(|\)|_)', '', i, flags=re.IGNORECASE)).strip() for i in gs_parsed]
+    movielist = list(dict.fromkeys(movielist))  # removing duplicates
+    if not movielist:
+        btn = [[
+            InlineKeyboardButton("⚠️ Instructions ⚠️", callback_data='instructions'),
+            InlineKeyboardButton("🔎 Search Google 🔍", url=f"https://www.google.com/search?q={search_google}")
+        ]]
+        k = await msg.reply_photo(photo=random.choice(PICS), caption=f"👋 Hello {msg.from_user.mention},\n\nI can't find the <b>'{search}'</b> you requested, Check if your spellings are correct. 😉", reply_markup=InlineKeyboardMarkup(btn))
+        await asyncio.sleep(60)
+        await k.delete()
+        try:
+            await msg.delete()
+        except:
+            pass
+        return
+    SPELL_CHECK[msg.id] = movielist
+    btn = [[
+        InlineKeyboardButton(
+            text=movie.strip(),
+            callback_data=f"spolling#{user}#{k}",
+        )
+    ] for k, movie in enumerate(movielist)]
+    btn.append([InlineKeyboardButton(text="❌ Close ❌", callback_data=f'close_data')])
+    k = await msg.reply_photo(photo=random.choice(PICS), caption=f"👋 Hello {msg.from_user.mention},\n\nI couldn't find the <b>'{search}'</b> you requested.\nDid you mean one of these? 👇",
                     reply_markup=InlineKeyboardMarkup(btn))
-
+    await asyncio.sleep(300)
+    await k.delete()
+    try:
+        await msg.delete()
+    except:
+        pass
