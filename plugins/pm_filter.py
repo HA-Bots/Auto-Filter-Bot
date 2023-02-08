057c1050a1eaa38f284e69ed83a0c27d776e44be
Hansaka Anuhas <121796740+Hansaka-Anuhas@users.noreply.github.com>
2023-02-08 08:24:37 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -836,6 +836,7 @@ async def auto_filter(client, msg, spoll=False):
                 pass
         else:
             await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=True)
+            return
     if spoll:
         await msg.message.delete()
 
