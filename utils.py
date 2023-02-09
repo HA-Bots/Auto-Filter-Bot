dbf208ced0c8996a9a0ef587fe0b80a90726e2e5
Hansaka Anuhas <121796740+Hansaka-Anuhas@users.noreply.github.com>
2023-02-09 11:40:10 +0530

    pyrogram v2 mistake fixed

--- utils.py
+++ utils.py
@@ -47,7 +47,7 @@ async def is_subscribed(bot, query):
     except Exception as e:
         logger.exception(e)
     else:
-        if user.status != 'kicked':
+        if user.status != enums.ChatMemberStatus.BANNED:
             return True
 
     return False
