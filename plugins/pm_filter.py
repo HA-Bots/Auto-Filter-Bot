1c89a87bc67fcde66a4cd61e1e0f0e18349c2b67
Hansaka Anuhas <121796740+Hansaka-Anuhas@users.noreply.github.com>
2023-02-11 09:47:50 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -811,7 +811,7 @@ async def auto_filter(client, msg, spoll=False):
             except:
                 pass
         else:
-            await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=True)
+            await msg.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=True)
 
 
 async def advantage_spell_chok(msg):
