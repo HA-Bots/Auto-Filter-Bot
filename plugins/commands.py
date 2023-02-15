e62e3af478488e849f8328e1029bb53057aa23fd
Hansaka Anuhas <121796740+Hansaka-Anuhas@users.noreply.github.com>
2023-02-15 10:42:35 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -162,15 +162,6 @@ async def stats(bot, message):
     size = get_size(size)
     free = get_size(free)
     await msg.edit(script.STATUS_TXT.format(files, users, chats, size, free))
-
-
-@Client.on_message(filters.command("ping"))
-async def ping(_, message):
-    start_t = time.time()
-    rm = await message.reply_text("...")
-    end_t = time.time()
-    time_taken_s = (end_t - start_t)
-    await rm.edit(f"Pong!\n{time_taken_s}")
     
     
 @Client.on_message(filters.command('settings'))
