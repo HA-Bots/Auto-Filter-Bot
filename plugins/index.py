6d83f511881250622665267dc0d5e84dc5979459
Hansaka Anuhas <121796740+Hansaka-Anuhas@users.noreply.github.com>
2023-04-04 11:20:21 +0530

    one more

--- plugins/index.py
+++ plugins/index.py
@@ -130,7 +130,7 @@ async def index_files_to_db(lst_msg_id, chat, msg, bot):
                 elif not message.media:
                     no_media += 1
                     continue
-                elif message.media not in [enums.MessageMediaType.DOCUMENT]:
+                elif message.media not in [enums.MessageMediaType.VIDEO, enums.MessageMediaType.DOCUMENT]:
                     unsupported += 1
                     continue
                 media = getattr(message, message.media.value, None)
