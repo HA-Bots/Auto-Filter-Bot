5f0bed8d2a12bd8aaee981f3333ad1757bb11a0e
Hansaka Anuhas <121796740+Hansaka-Anuhas@users.noreply.github.com>
2023-04-21 12:24:45 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -1,7 +1,6 @@
 import os
 from pyrogram import Client, filters, enums
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
-from utils import extract_user, get_file_id, get_poster, last_online
 import time
 from datetime import datetime
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
