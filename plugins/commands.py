2e9692363f0151187ea2207c62d1aa01f39d1857
Hansaka Anuhas <121796740+Hansaka-Anuhas@users.noreply.github.com>
2023-04-21 12:39:14 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -63,7 +63,7 @@ async def start(client, message):
             InlineKeyboardButton("📢 Updates Channel 📢", url=invite_link.invite_link)
         ]]
 
-        if message.command[1] != "subscribe" or message.command[1] != "all":
+        if message.command[1] != "subscribe" and message.command[1] != "all":
             try:
                 kk, file_id = message.command[1].split("_", 1)
                 pre = 'checksubp' if kk == 'filep' else 'checksub' 
