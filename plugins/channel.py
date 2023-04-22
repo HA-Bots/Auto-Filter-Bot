302d646477017a58079194ed6659913e6618da88
Hansaka Anuhas <121796740+Hansaka-Anuhas@users.noreply.github.com>
2023-04-22 18:26:21 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -10,8 +10,6 @@ async def media(bot, message):
     """Media Handler"""
     media = getattr(message, message.media.value, None)
     if media.mime_type in ['video/mp4', 'video/x-matroska']:
-
-
         media.file_type = message.media.value
         media.caption = message.caption
         await save_file(media)
