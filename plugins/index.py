ff3d254039becccddcfb6eceaf5c5127f32e71c7
Hansaka Anuhas <121796740+Hansaka-Anuhas@users.noreply.github.com>
2023-04-22 16:39:26 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -134,7 +134,7 @@ async def index_files_to_db(lst_msg_id, chat, msg, bot):
                     unsupported += 1
                     continue
                 media = getattr(message, message.media.value, None)
-                message.media.value
+                print(message.media.value)
                 if not media:
                     unsupported += 1
                     continue
