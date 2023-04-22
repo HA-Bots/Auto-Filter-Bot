48f5adeb4584f2e9382068da61ced7bd3f42021f
technical_aks <105633032+technicalaks@users.noreply.github.com>
2023-04-22 14:39:53 +0530

    Mistake 😅

--- plugins/inline.py
+++ plugins/inline.py
@@ -4,7 +4,7 @@ from pyrogram.errors.exceptions.bad_request_400 import QueryIdInvalid
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultCachedDocument, InlineQuery
 from database.ia_filterdb import get_search_results
 from utils import is_subscribed, get_size, temp
-from info import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION, SUPPORT_LINK, UPDATES_LINK
+from info import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, SUPPORT_LINK, UPDATES_LINK
 
 logger = logging.getLogger(__name__)
 cache_time = 0 if AUTH_USERS or AUTH_CHANNEL else CACHE_TIME
