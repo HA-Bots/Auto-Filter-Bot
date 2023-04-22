5da4aa43be396a33401f878e4ef30f8fcaa7284c
Hansaka Anuhas <121796740+Hansaka-Anuhas@users.noreply.github.com>
2023-04-22 18:15:53 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -8,16 +8,10 @@ media_filter = filters.document | filters.video
 @Client.on_message(filters.chat(INDEX_CHANNELS) & media_filter)
 async def media(bot, message):
     """Media Handler"""
-    for file_type in ("document", "video"):
-        media = getattr(message, file_type, None)
-        print(media)
-        if media is not None:
-            break
-            print('x')
-        print('b')
-    else:
-        return
+    if message.mime_type in ['video/mp4', 'video/x-matroska']:
+        media = getattr(message, message.media.value, None)
 
-    media.file_type = file_type
-    media.caption = message.caption
-    await save_file(media)
+
+        media.file_type = file_type
+        media.caption = message.caption
+        await save_file(media)
