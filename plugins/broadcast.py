4ab4523c849d6ec45a06a9dc93e5ddbcea644cfc
Hansaka Anuhas <121796740+Hansaka-Anuhas@users.noreply.github.com>
2023-05-05 16:13:33 +0530

    add broadcast cancel feature

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -3,8 +3,20 @@ import datetime
 import time
 from database.users_chats_db import db
 from info import ADMINS
-from utils import broadcast_messages, groups_broadcast_messages
+from utils import broadcast_messages, groups_broadcast_messages, temp
 import asyncio
+        
+        
+@Client.on_callback_query(filters.regex(r'^broadcast_cancel'))
+async def broadcast_cancel(bot, query):
+    _, ident = query.data.split("#")
+    if ident == 'users':
+        await query.message.edit("Trying to cancel users broadcasting...")
+        temp.USERS_CANCEL = True
+    elif ident == 'groups':
+        temp.GROUPS_CANCEL = True
+        await query.message.edit("Trying to cancel groups broadcasting...")
+        
         
 @Client.on_message(filters.command("users_broadcast") & filters.user(ADMINS) & filters.reply)
 async def users_broadcast(bot, message):
@@ -19,9 +31,13 @@ async def users_broadcast(bot, message):
     blocked = 0
     deleted = 0
     failed =0
+    temp.USERS_CANCEL = False
 
     success = 0
     async for user in users:
+        if temp.USERS_CANCEL:
+            await msg.edit(f"Users Successfully Broadcast Canceled!")
+            break
         pti, sh = await broadcast_messages(int(user['id']), b_msg)
         if pti:
             success += 1
@@ -35,7 +51,10 @@ async def users_broadcast(bot, message):
         done += 1
         await asyncio.sleep(2)
         if not done % 20:
-            await sts.edit(f"Users broadcast in progress...\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>\nBlocked: <code>{blocked}</code>\nDeleted: <code>{deleted}</code>")    
+            btn = [[
+                InlineKeyboardButton('CANCEL', callback_data=f'broadcast_cancel#users')
+            ]]
+            await sts.edit(f"Users broadcast in progress...\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>\nBlocked: <code>{blocked}</code>\nDeleted: <code>{deleted}</code>", reply_markup=InlineKeyboardMarkup(btn))    
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
     await sts.edit(f"Users broadcast completed.\nCompleted in {time_taken} seconds.\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>\nBlocked: <code>{blocked}</code>\nDeleted: <code>{deleted}</code>")
         
@@ -51,9 +70,13 @@ async def groups_broadcast(bot, message):
     total_chats = await db.total_chat_count()
     done = 0
     failed =0
+    temp.GROUPS_CANCEL = False
 
     success = 0
     async for chat in chats:
+        if temp.GROUPS_CANCEL:
+            await msg.edit(f"Groups Successfully Broadcast Canceled!")
+            break
         pti, sh = await groups_broadcast_messages(int(chat['id']), b_msg)
         if pti:
             success += 1
@@ -63,6 +86,9 @@ async def groups_broadcast(bot, message):
         done += 1
         await asyncio.sleep(2)
         if not done % 20:
-            await sts.edit(f"Groups groadcast in progress...\n\nTotal Groups: <code>{total_chats}</code>\nCompleted: <code>{done} / {total_chats}</code>\nSuccess: <code>{success}</code>\nFailed: <code>{failed}</code>")    
+            btn = [[
+                InlineKeyboardButton('CANCEL', callback_data=f'broadcast_cancel#groups')
+            ]]
+            await sts.edit(f"Groups groadcast in progress...\n\nTotal Groups: <code>{total_chats}</code>\nCompleted: <code>{done} / {total_chats}</code>\nSuccess: <code>{success}</code>\nFailed: <code>{failed}</code>", reply_markup=InlineKeyboardMarkup(btn))    
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
     await sts.edit(f"Groups broadcast completed.\nCompleted in {time_taken} seconds.\n\nTotal Groups: <code>{total_chats}</code>\nCompleted: <code>{done} / {total_chats}</code>\nSuccess: <code>{success}</code>\nFailed: <code>{failed}</code>")
