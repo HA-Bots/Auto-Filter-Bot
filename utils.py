44384afdaa6a41442281329c889dea88b4a143f2
technical_aks <105633032+technicalaks@users.noreply.github.com>
2023-05-22 19:34:12 +0530

    Group id not show error fixed

--- utils.py
+++ utils.py
@@ -214,3 +214,26 @@ async def get_shortlink(group_id, link):
 
     link = await shortzy.convert(link)
     return link
+
+
+def get_file_id(msg: Message):
+    if msg.media:
+        for message_type in (
+            "photo",
+            "animation",
+            "audio",
+            "document",
+            "video",
+            "video_note",
+            "voice",
+            "sticker"
+        ):
+            obj = getattr(msg, message_type)
+            if obj:
+                setattr(obj, "message_type", message_type)
+                return obj
+            
+            
+            
+            
+            
