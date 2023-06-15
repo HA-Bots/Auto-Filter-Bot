3818b9569f403a7f4e8722ca743bafd498fe7076
Hansaka Anuhas <121796740+Hansaka-Anuhas@users.noreply.github.com>
2023-06-15 14:48:57 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -110,7 +110,29 @@ async def get_search_results(query, file_type=None, max_results=10, offset=0, fi
 
     return files, next_offset, total_results
     
+
+async def delete_files(query, filter=True):
+    query = query.strip()
+    # for better results
+    if filter:
+        query = query.replace(' ', r'(\s|\.|\+|\-|_)')
+        raw_pattern = r'(\s|_|\-|\.|\+)' + query + r'(\s|_|\-|\.|\+)'
+    if not query:
+        raw_pattern = '.'
+    elif ' ' not in query:
+        raw_pattern = r'(\b|[\.\+\-_])' + query + r'(\b|[\.\+\-_])'
+    else:
+        raw_pattern = query.replace(' ', r'.*[\s\.\+\-_]')
     
+    try:
+        regex = re.compile(raw_pattern, flags=re.IGNORECASE)
+    except:
+        return []
+    filter = {'file_name': regex}
+    total = await Media.count_documents(filter)
+    files = Media.find(filter)
+    return total, files
+
 async def get_file_details(query):
     filter = {'file_id': query}
     cursor = Media.find(filter)
