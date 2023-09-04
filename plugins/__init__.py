05fea7ad19516aefc02ae1b7c5cc6eac03d17253
Hansaka Anuhas <141299098+HansakaAnuhas-TG@users.noreply.github.com>
2023-09-04 17:13:25 +0530

    .......

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,5 +1,7 @@
 from aiohttp import web
 
+routes = web.RouteTableDef()
+
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
     return web.json_response(text="@SL_Bots_Updates")
