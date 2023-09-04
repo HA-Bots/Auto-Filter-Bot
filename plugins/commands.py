a110411bbfb459dc9dd49185965a6458aaf4cfeb
Hansaka Anuhas <141299098+HansakaAnuhas-TG@users.noreply.github.com>
2023-09-04 16:25:16 +0530

    started bug/error fixing session

--- plugins/commands.py
+++ plugins/commands.py
@@ -116,7 +116,7 @@ async def start(client, message):
                 file_caption=file.caption
             )   
             btn = [[
-                InlineKeyboardButton("✛ ᴡᴀᴛᴄʜ & ᴅᴏᴡɴʟᴏᴀᴅ ✛", callback_data=f"stream#{file_id}")
+                InlineKeyboardButton("✛ ᴡᴀᴛᴄʜ & ᴅᴏᴡɴʟᴏᴀᴅ ✛", callback_data=f"stream#{file.file_id}")
             ]]
             await client.send_cached_media(
                 chat_id=message.from_user.id,
