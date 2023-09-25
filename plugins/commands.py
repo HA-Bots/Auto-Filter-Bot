345377b16f967bfc4fdd984d2c5323aaed61b2b3
Hansaka Anuhas <141299098+HansakaAnuhas-TG@users.noreply.github.com>
2023-09-25 15:02:40 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -42,7 +42,7 @@ async def start(client, message):
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.NEW_USER_TXT.format(message.from_user.mention, message.from_user.id))
-    if (len(message.command) != 2) or (len(message.command) != 2 and message.command[1] == 'start'):
+    if (len(message.command) != 2) or (len(message.command) == 2 and message.command[1] == 'start'):
         buttons = [[
             InlineKeyboardButton("+ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ +", url=f'http://t.me/{temp.U_NAME}?startgroup=start')
         ],[
