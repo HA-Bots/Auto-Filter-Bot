578a0202dc4983cf1bb03cd51dadf78403710eec
Hansaka Anuhas <141299098+HansakaAnuhas-TG@users.noreply.github.com>
2023-09-27 13:12:25 +0530

    minor fixes

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -30,34 +30,29 @@ async def users_broadcast(bot, message):
     total_users = await db.total_users_count()
     done = 0
     blocked = 0
-    deleted = 0
     failed =0
     temp.USERS_CANCEL = False
-
     success = 0
+        
     async for user in users:
         if temp.USERS_CANCEL:
             await msg.edit(f"Users Successfully Broadcast Canceled!")
             break
-        pti, sh = await broadcast_messages(int(user['id']), b_msg)
-        if pti:
+        sts = await broadcast_messages(int(user['id']), b_msg)
+        if sts == 'Success':
             success += 1
-        elif pti == False:
-            if sh == "Blocked":
-                blocked+=1
-            elif sh == "Deleted":
-                deleted += 1
-            elif sh == "Error":
-                failed += 1
+        elif sts == 'Blocked':
+            blocked+=1
+        elif sts == 'Error':
+            failed += 1
         done += 1
-        await asyncio.sleep(2)
         if not done % 20:
             btn = [[
                 InlineKeyboardButton('CANCEL', callback_data=f'broadcast_cancel#users')
             ]]
-            await sts.edit(f"Users broadcast in progress...\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>\nBlocked: <code>{blocked}</code>\nDeleted: <code>{deleted}</code>", reply_markup=InlineKeyboardMarkup(btn))    
+            await sts.edit(f"Users broadcast in progress...\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>\nBlocked: <code>{blocked}</code>", reply_markup=InlineKeyboardMarkup(btn))    
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-    await sts.edit(f"Users broadcast completed.\nCompleted in {time_taken} seconds.\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>\nBlocked: <code>{blocked}</code>\nDeleted: <code>{deleted}</code>")
+    await sts.edit(f"Users broadcast completed.\nCompleted in {time_taken} seconds.\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>\nBlocked: <code>{blocked}</code>")
         
 
 @Client.on_message(filters.command("groups_broadcast") & filters.user(ADMINS) & filters.reply)
@@ -72,20 +67,18 @@ async def groups_broadcast(bot, message):
     done = 0
     failed =0
     temp.GROUPS_CANCEL = False
-
     success = 0
+        
     async for chat in chats:
         if temp.GROUPS_CANCEL:
             await msg.edit(f"Groups Successfully Broadcast Canceled!")
             break
-        pti, sh = await groups_broadcast_messages(int(chat['id']), b_msg)
-        if pti:
+        sts = await groups_broadcast_messages(int(chat['id']), b_msg)
+        if sts == 'Success':
             success += 1
-        elif pti == False:
-            if sh == "Error":
-                failed += 1
+        elif sts == 'Error':
+            failed += 1
         done += 1
-        await asyncio.sleep(2)
         if not done % 20:
             btn = [[
                 InlineKeyboardButton('CANCEL', callback_data=f'broadcast_cancel#groups')
