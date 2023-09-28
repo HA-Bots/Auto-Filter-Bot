21e51ef99b4f160337a8b36fd82d8701ed050115
Hansaka Anuhas <141299098+HansakaAnuhas-TG@users.noreply.github.com>
2023-09-28 15:27:09 +0530

    fixes

--- web/__init__.py
+++ web/__init__.py
@@ -1,12 +1,8 @@
 # Credit - adarsh-goel
 
 from aiohttp import web
+from web.stream_routes import routes
 
-routes = web.RouteTableDef()
-
-@routes.get("/", allow_head=True)
-async def root_route_handler(request):
-    return web.json_response(text="@SL_Bots_Updates")
 
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
