00d9db5c793f72156bddc681abace92f05f0fb3b
technical_aks <105633032+technicalaks@users.noreply.github.com>
2023-09-29 16:50:35 +0530

    Update broadcast.py
    Change command.....
    Now,
    For users broadcast - /broadcast
    For groups broadcast - /grp_broadcast

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -6,8 +6,7 @@ from info import ADMINS
 from utils import broadcast_messages, groups_broadcast_messages, temp
 import asyncio
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-        
-        
+
 @Client.on_callback_query(filters.regex(r'^broadcast_cancel'))
 async def broadcast_cancel(bot, query):
     _, ident = query.data.split("#")
@@ -17,9 +16,8 @@ async def broadcast_cancel(bot, query):
     elif ident == 'groups':
         temp.GROUPS_CANCEL = True
         await query.message.edit("Trying to cancel groups broadcasting...")
-        
-        
-@Client.on_message(filters.command("users_broadcast") & filters.user(ADMINS) & filters.reply)
+               
+@Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
 async def users_broadcast(bot, message):
     users = await db.get_all_users()
     b_msg = message.reply_to_message
@@ -54,8 +52,7 @@ async def users_broadcast(bot, message):
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
     await sts.edit(f"Users broadcast completed.\nCompleted in {time_taken} seconds.\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>\nBlocked: <code>{blocked}</code>")
         
-
-@Client.on_message(filters.command("groups_broadcast") & filters.user(ADMINS) & filters.reply)
+@Client.on_message(filters.command("grp_broadcast") & filters.user(ADMINS) & filters.reply)
 async def groups_broadcast(bot, message):
     chats = await db.get_all_chats()
     b_msg = message.reply_to_message
@@ -86,3 +83,5 @@ async def groups_broadcast(bot, message):
             await sts.edit(f"Groups groadcast in progress...\n\nTotal Groups: <code>{total_chats}</code>\nCompleted: <code>{done} / {total_chats}</code>\nSuccess: <code>{success}</code>\nFailed: <code>{failed}</code>", reply_markup=InlineKeyboardMarkup(btn))    
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
     await sts.edit(f"Groups broadcast completed.\nCompleted in {time_taken} seconds.\n\nTotal Groups: <code>{total_chats}</code>\nCompleted: <code>{done} / {total_chats}</code>\nSuccess: <code>{success}</code>\nFailed: <code>{failed}</code>")
+
+
