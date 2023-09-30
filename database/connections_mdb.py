fc8dd6aa4c8bf653222bf86711642f4bcd8ab2a9
technical_aks <105633032+technicalaks@users.noreply.github.com>
2023-09-30 13:59:25 +0530

    Update connections_mdb.py
    ♻️ Optimization...

--- database/connections_mdb.py
+++ database/connections_mdb.py
@@ -1,15 +1,13 @@
 import pymongo
-
 from info import DATABASE_URL, DATABASE_NAME
-
 import logging
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
 myclient = pymongo.MongoClient(DATABASE_URL)
 mydb = myclient[DATABASE_NAME]
 mycol = mydb['Connections']   
-
 
 async def add_connection(group_id, user_id):
     query = mycol.find_one(
@@ -37,7 +35,6 @@ async def add_connection(group_id, user_id):
             return True
         except:
             logger.exception('Some error occurred!', exc_info=True)
-
     else:
         try:
             mycol.update_one(
@@ -50,8 +47,7 @@ async def add_connection(group_id, user_id):
             return True
         except:
             logger.exception('Some error occurred!', exc_info=True)
-
-        
+      
 async def active_connection(user_id):
 
     query = mycol.find_one(
@@ -64,7 +60,6 @@ async def active_connection(user_id):
     group_id = query['active_group']
     return int(group_id) if group_id != None else None
 
-
 async def all_connections(user_id):
     query = mycol.find_one(
         { "_id": user_id },
@@ -75,14 +70,12 @@ async def all_connections(user_id):
     else:
         return None
 
-
 async def if_active(user_id, group_id):
     query = mycol.find_one(
         { "_id": user_id },
         { "_id": 0, "group_details": 0 }
     )
     return query is not None and query['active_group'] == group_id
-
 
 async def make_active(user_id, group_id):
     update = mycol.update_one(
@@ -91,14 +84,12 @@ async def make_active(user_id, group_id):
     )
     return update.modified_count != 0
 
-
 async def make_inactive(user_id):
     update = mycol.update_one(
         {'_id': user_id},
         {"$set": {"active_group" : None}}
     )
     return update.modified_count != 0
-
 
 async def delete_connection(user_id, group_id):
 
@@ -130,4 +121,3 @@ async def delete_connection(user_id, group_id):
     except Exception as e:
         logger.exception(f'Some error occurred! {e}', exc_info=True)
         return False
-
