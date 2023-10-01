6ba36d53097037a248b7b10fdfca605806dfab0c
Hansaka Anuhas <141299098+HansakaAnuhas-TG@users.noreply.github.com>
2023-10-01 11:57:04 +0530

    better?

--- plugins/telegraph.py
+++ plugins/telegraph.py
@@ -1,22 +1,11 @@
 import os
-import asyncio
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from telegraph import upload_file
-from utils import get_file_id
 
-@Client.on_message(filters.private & (filters.command("telegraph") | filters.photo))
+@Client.on_message(filters.private & filters.photo)
 async def telegraph_upload(bot, message):
-    replied = message.reply_to_message
-    if not replied:
-        await message.reply_text("⚠️ ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴘʜᴏᴛᴏ ᴏʀ ᴠɪᴅᴇᴏ ᴜɴᴅᴇʀ 5 ᴍʙ")
-        return
-    file_info = get_file_id(replied)
-    if not file_info:
-        await message.reply_text("⁉️ ɴᴏᴛ sᴜᴘᴘᴏʀᴛᴇᴅ 😑")
-        return
     text = await message.reply_text(text="<code>ᴘʀᴏᴄᴇssɪɴɢ....</code>")   
-    media = await message.reply_to_message.download()  
+    media = await message.download()  
     try:
         response = upload_file(media)
     except Exception as e:
@@ -26,17 +15,5 @@ async def telegraph_upload(bot, message):
         os.remove(media)
     except:
         pass
-    await text.delete()
-    await message.reply_photo(
-        photo=f'https://telegra.ph/{response[0]}',
-        caption=f"<b>❤️ ʏᴏᴜʀ ᴛᴇʟᴇɢʀᴀᴘʜ ʟɪɴᴋ ᴄᴏᴍᴘʟᴇᴛᴇᴅ 👇</b>\n\n<code>https://telegra.ph/{response[0]}</code></b>",       
-        reply_markup=InlineKeyboardMarkup(
-          [[
-            InlineKeyboardButton(text="✅ ᴏᴘᴇɴ ʟɪɴᴋ ✅", url=f"https://telegra.ph/{response[0]}"),
-            InlineKeyboardButton(text="🔁 sʜᴀʀᴇ ʟɪɴᴋ 🔁", url=f"https://telegram.me/share/url?url=https://telegra.ph/{response[0]}")
-          ],[
-            InlineKeyboardButton(text="❌ ᴄʟᴏsᴇ ❌", callback_data="close_data")
-          ]]
-        )
-    )
+    await text.edit_text(f"<b>❤️ ʏᴏᴜʀ ᴛᴇʟᴇɢʀᴀᴘʜ ʟɪɴᴋ ᴄᴏᴍᴘʟᴇᴛᴇᴅ 👇</b>\n\n<code>https://telegra.ph/{response[0]}</code></b>")
 
