34beb750821394ac79e9fbfb87582018ec32445a
Hansaka Anuhas <141299098+HansakaAnuhas-TG@users.noreply.github.com>
2023-10-01 11:45:47 +0530

    minor fixes

--- plugins/telegraph.py
+++ plugins/telegraph.py
@@ -1,41 +1,39 @@
 import os
 import asyncio
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from telegraph import upload_file
 from utils import get_file_id
 
-@Client.on_message(filters.command("telegraph") & filters.private)
-async def telegraph_upload(bot, update):
-    replied = update.reply_to_message
+@Client.on_message(filters.private & (filters.command("telegraph") | filters.photo))
+async def telegraph_upload(bot, message):
+    replied = message.reply_to_message
     if not replied:
-        await update.reply_text("⚠️ ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴘʜᴏᴛᴏ ᴏʀ ᴠɪᴅᴇᴏ ᴜɴᴅᴇʀ 5 ᴍʙ")
+        await message.reply_text("⚠️ ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴘʜᴏᴛᴏ ᴏʀ ᴠɪᴅᴇᴏ ᴜɴᴅᴇʀ 5 ᴍʙ")
         return
     file_info = get_file_id(replied)
     if not file_info:
-        await update.reply_text("⁉️ ɴᴏᴛ sᴜᴘᴘᴏʀᴛᴇᴅ 😑")
+        await message.reply_text("⁉️ ɴᴏᴛ sᴜᴘᴘᴏʀᴛᴇᴅ 😑")
         return
-    text = await update.reply_text(text="<code>ᴘʀᴏᴄᴇssɪɴɢ....</code>", disable_web_page_preview=True)   
-    media = await update.reply_to_message.download()   
-    await text.edit_text(text="<code>ᴅᴏɴᴇ :)</code>", disable_web_page_preview=True)                                            
+    text = await message.reply_text(text="<code>ᴘʀᴏᴄᴇssɪɴɢ....</code>")   
+    media = await message.reply_to_message.download()  
     try:
         response = upload_file(media)
-    except Exception as error:
-        print(error)
-        await text.edit_text(text=f"Error :- {error}", disable_web_page_preview=True)       
+    except Exception as e:
+        await text.edit_text(text=f"Error :- {e}")
         return    
     try:
         os.remove(media)
-    except Exception as error:
-        print(error)
-        return    
-    await update.reply_photo(
-        photo=f'https://graph.org{response[0]}',
-        caption=f"<b>❤️ ʏᴏᴜʀ ᴛᴇʟᴇɢʀᴀᴘʜ ʟɪɴᴋ ᴄᴏᴍᴘʟᴇᴛᴇᴅ 👇</b>\n\n<code>https://graph.org{response[0]}</code></b>",       
+    except:
+        pass
+    await text.delete()
+    await message.reply_photo(
+        photo=f'https://telegra.ph/{response[0]}',
+        caption=f"<b>❤️ ʏᴏᴜʀ ᴛᴇʟᴇɢʀᴀᴘʜ ʟɪɴᴋ ᴄᴏᴍᴘʟᴇᴛᴇᴅ 👇</b>\n\n<code>https://telegra.ph/{response[0]}</code></b>",       
         reply_markup=InlineKeyboardMarkup(
           [[
-            InlineKeyboardButton(text="✅ ᴏᴘᴇɴ ʟɪɴᴋ ✅", url=f"https://graph.org{response[0]}"),
-            InlineKeyboardButton(text="🔁 sʜᴀʀᴇ ʟɪɴᴋ 🔁", url=f"https://telegram.me/share/url?url=https://graph.org{response[0]}")
+            InlineKeyboardButton(text="✅ ᴏᴘᴇɴ ʟɪɴᴋ ✅", url=f"https://telegra.ph/{response[0]}"),
+            InlineKeyboardButton(text="🔁 sʜᴀʀᴇ ʟɪɴᴋ 🔁", url=f"https://telegram.me/share/url?url=https://telegra.ph/{response[0]}")
           ],[
             InlineKeyboardButton(text="❌ ᴄʟᴏsᴇ ❌", callback_data="close_data")
           ]]
