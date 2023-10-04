b5602ea9b7fd6d4cf82fe9830daf7679e21541bd
Hansaka Anuhas <141299098+HansakaAnuhas-TG@users.noreply.github.com>
2023-10-04 11:54:15 +0530

    private channel index with SESSION_STRING

--- utils.py
+++ utils.py
@@ -11,7 +11,7 @@ import os
 import pytz
 import time
 from datetime import datetime
-from typing import List, Any
+from typing import List, Any, Union, Optional, AsyncGenerator
 from database.users_chats_db import db
 from bs4 import BeautifulSoup
 import requests
@@ -38,6 +38,7 @@ class temp(object):
     USERS_CANCEL = False
     GROUPS_CANCEL = False
     BOT = None
+    USER_BOT = None
 
 async def is_subscribed(bot, query, channel=AUTH_CHANNEL):
     btn = []
@@ -130,6 +131,42 @@ async def get_poster(query, bulk=False, id=False, file=None):
         'rating': str(movie.get("rating")),
         'url':f'https://www.imdb.com/title/tt{movieid}'
     }
+
+
+async def iter_messages(client, chat_id: Union[int, str], limit: int, offset: int = 0) -> Optional[AsyncGenerator["types.Message", None]]:
+    """Iterate through a chat sequentially.
+    This convenience method does the same as repeatedly calling :meth:`~pyrogram.Client.get_messages` in a loop, thus saving
+    you from the hassle of setting up boilerplate code. It is useful for getting the whole chat messages with a
+    single call.
+    Parameters:
+        chat_id (``int`` | ``str``):
+            Unique identifier (int) or username (str) of the target chat.
+            For your personal cloud (Saved Messages) you can simply use "me" or "self".
+            For a contact that exists in your Telegram address book you can use his phone number (str).
+                
+        limit (``int``):
+            Identifier of the last message to be returned.
+                
+        offset (``int``, *optional*):
+            Identifier of the first message to be returned.
+            Defaults to 0.
+    Returns:
+        ``Generator``: A generator yielding :obj:`~pyrogram.types.Message` objects.
+    Example:
+        .. code-block:: python
+            for message in app.iter_messages("pyrogram", 1, 15000):
+                print(message.text)
+    """
+    current = offset
+    while True:
+        new_diff = min(200, limit - current)
+        if new_diff <= 0:
+            return
+        messages = await client.get_messages(chat_id, list(range(current, current+new_diff+1)))
+        for message in messages:
+            yield message
+            current += 1
+
 
 async def is_check_admin(bot, chat_id, user_id):
     try:
