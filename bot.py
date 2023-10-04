db887e60f9393d242bf508b603ae654f8f217843
Hansaka Anuhas <141299098+HansakaAnuhas-TG@users.noreply.github.com>
2023-10-04 11:55:03 +0530

    private channel index with SESSION_STRING

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ from database.ia_filterdb import Media
 from aiohttp import web
 from database.users_chats_db import db
 from web import web_server
-from info import SESSION, LOG_CHANNEL, API_ID, API_HASH, BOT_TOKEN, PORT, BIN_CHANNEL
+from info import SESSION_STRING, LOG_CHANNEL, API_ID, API_HASH, BOT_TOKEN, PORT, BIN_CHANNEL
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
@@ -22,10 +22,9 @@ from pyrogram.errors import AccessTokenExpired, AccessTokenInvalid
 
 
 class Bot(Client):
-
     def __init__(self):
         super().__init__(
-            name=SESSION,
+            name='Auto_Filter_Bot',
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
@@ -44,6 +43,19 @@ class Bot(Client):
         except (AccessTokenExpired, AccessTokenInvalid):
             logging.error("Your BOT_TOKEN revoke and add again, exiting now")
             exit()
+        if len(SESSION_STRING) != 0:
+            user_bot = Client(
+                name='Auto_Filter_User_Bot',
+                api_id=API_ID,
+                api_hash=API_HASH,
+                session_string=SESSION_STRING
+            )
+            try:
+                await user_bot.start()
+                temp.USER_BOT = user_bot
+            except:
+                logging.error("Your SESSION_STRING revoke and add again, exiting now")
+                exit()
         if os.path.exists('restart.txt'):
             with open("restart.txt") as file:
                 chat_id, msg_id = map(int, file)
@@ -79,45 +91,6 @@ class Bot(Client):
     async def stop(self, *args):
         await super().stop()
         logging.info("Bot stopped! Bye...")
-    
-    async def iter_messages(
-        self,
-        chat_id: Union[int, str],
-        limit: int,
-        offset: int = 0,
-    ) -> Optional[AsyncGenerator["types.Message", None]]:
-        """Iterate through a chat sequentially.
-        This convenience method does the same as repeatedly calling :meth:`~pyrogram.Client.get_messages` in a loop, thus saving
-        you from the hassle of setting up boilerplate code. It is useful for getting the whole chat messages with a
-        single call.
-        Parameters:
-            chat_id (``int`` | ``str``):
-                Unique identifier (int) or username (str) of the target chat.
-                For your personal cloud (Saved Messages) you can simply use "me" or "self".
-                For a contact that exists in your Telegram address book you can use his phone number (str).
-                
-            limit (``int``):
-                Identifier of the last message to be returned.
-                
-            offset (``int``, *optional*):
-                Identifier of the first message to be returned.
-                Defaults to 0.
-        Returns:
-            ``Generator``: A generator yielding :obj:`~pyrogram.types.Message` objects.
-        Example:
-            .. code-block:: python
-                for message in app.iter_messages("pyrogram", 1, 15000):
-                    print(message.text)
-        """
-        current = offset
-        while True:
-            new_diff = min(200, limit - current)
-            if new_diff <= 0:
-                return
-            messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
-            for message in messages:
-                yield message
-                current += 1
 
 
 app = Bot()
