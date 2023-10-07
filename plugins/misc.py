43372e217f36c087c719549eca080142875dd35f
Hansaka Anuhas <141299098+HansakaAnuhas-TG@users.noreply.github.com>
2023-10-07 16:46:47 +0530

    -

--- plugins/misc.py
+++ plugins/misc.py
@@ -1,5 +1,4 @@
 import os
-from utils import get_file_id
 from pyrogram import Client, filters, enums
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 import time
@@ -13,44 +12,12 @@ logger.setLevel(logging.ERROR)
 async def showid(client, message):
     chat_type = message.chat.type
     if chat_type == enums.ChatType.PRIVATE:
-        user_id = message.chat.id
-        first = message.from_user.first_name
-        last = message.from_user.last_name or "None"
-        username = f"@{message.from_user.username}" or "None"
-        await message.reply_text(
-            f"★ First Name: {first}\n★ Last Name: {last}\n★ Username: {username}\n★ User ID: <code>{user_id}</code>",
-            quote=True
-        )
+        await message.reply_text(f'★ User ID: <code>{message.from_user.id}</code>')
 
     elif chat_type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
-        _id = ""
-        _id += (
-            "★ Chat ID: "
-            f"<code>{message.chat.id}</code>\n"
-        )
-        if message.reply_to_message:
-            _id += (
-                "★ User ID: "
-                f"<code>{message.from_user.id if message.from_user else 'Anonymous'}</code>\n"
-                "★ Replied User ID: "
-                f"<code>{message.reply_to_message.from_user.id if message.reply_to_message.from_user else 'Anonymous'}</code>\n"
-            )
-            file_info = get_file_id(message.reply_to_message)
-        else:
-            _id += (
-                "★ User ID: "
-                f"<code>{message.from_user.id if message.from_user else 'Anonymous'}</code>\n"
-            )
-            file_info = get_file_id(message)
-        if file_info:
-            _id += (
-                f"★ {file_info.message_type}: "
-                f"<code>{file_info.file_id}</code>\n"
-            )
-        await message.reply_text(
-            _id,
-            quote=True
-        )
+        await message.reply_text(f'★ Group ID: <code>{message.chat.id}</code>')
 
     elif chat_type == enums.ChatType.CHANNEL:
         await message.reply_text(f'★ Channel ID: <code>{message.chat.id}</code>')
+
+
