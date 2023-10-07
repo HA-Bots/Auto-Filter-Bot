a1c7d3e38a1151c51c150ee243d8e66633a5695a
Hansaka Anuhas <141299098+HansakaAnuhas-TG@users.noreply.github.com>
2023-10-07 16:29:04 +0530

    `/eval` command

---
+++ plugins/eval.py
@@ -0,0 +1,51 @@
+from pyrogram import Client, filters
+from pyrogram.errors import MessageTooLong
+import sys
+import re
+import traceback
+from io import StringIO
+from info import ADMINS
+
+@Client.on_message(filters.command("eval") & filters.user(ADMINS))
+async def executor(client, message):
+    try:
+        cmd = message.text.split(" ", 1)[1]
+    except:
+        return await message.reply('Command Incomplete!')
+    old_stderr = sys.stderr
+    old_stdout = sys.stdout
+    redirected_output = sys.stdout = StringIO()
+    redirected_error = sys.stderr = StringIO()
+    stdout, stderr, exc = None, None, None
+    try:
+        await aexec(cmd, client, message)
+    except:
+        exc = traceback.format_exc()
+    stdout = redirected_output.getvalue()
+    stderr = redirected_error.getvalue()
+    sys.stdout = old_stdout
+    sys.stderr = old_stderr
+    evaluation = ""
+    if exc:
+        evaluation = exc
+    elif stderr:
+        evaluation = stderr
+    elif stdout:
+        evaluation = stdout
+    else:
+        evaluation = "Success!"
+    final_output = f"Output:\n\n<code>{evaluation}</code>"
+    try:
+        await message.reply(final_output)
+    except MessageTooLong:
+        with open('eval.txt', 'w+') as outfile:
+            outfile.write(final_output)
+        await message.reply_document('eval.txt')
+
+
+async def aexec(code, client, message):
+    exec(
+        "async def __aexec(client, message): "
+        + "".join(f"\n {a}" for a in code.split("\n"))
+    )
+    return await locals()["__aexec"](client, message)
