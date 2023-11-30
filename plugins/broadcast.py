aa2b981dd91fb1b99bfbbab39734045b4a91aa1b
Hansaka Anuhas <141299098+HansakaAnuhas-TG@users.noreply.github.com>
2023-11-30 19:22:23 +0530

    fixes

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -21,20 +21,20 @@ async def broadcast_cancel(bot, query):
 async def users_broadcast(bot, message):
     users = await db.get_all_users()
     b_msg = message.reply_to_message
-    sts = await message.reply_text(
+    b_sts = await message.reply_text(
         text='Broadcasting your users messages...'
     )
     start_time = time.time()
     total_users = await db.total_users_count()
     done = 0
-    failed =0
+    failed = 0
     temp.USERS_CANCEL = False
     success = 0
         
     async for user in users:
         time_taken = get_readable_time(time.time()-start_time)
         if temp.USERS_CANCEL:
-            await sts.edit(f"Users broadcast Cancelled!\nCompleted in {time_taken}\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>")
+            await b_sts.edit(f"Users broadcast Cancelled!\nCompleted in {time_taken}\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>")
             return
         sts = await broadcast_messages(int(user['id']), b_msg)
         if sts == 'Success':
@@ -46,28 +46,28 @@ async def users_broadcast(bot, message):
             btn = [[
                 InlineKeyboardButton('CANCEL', callback_data=f'broadcast_cancel#users')
             ]]
-            await sts.edit(f"Users broadcast in progress...\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>", reply_markup=InlineKeyboardMarkup(btn))
-    await sts.edit(f"Users broadcast completed.\nCompleted in {time_taken}\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>")
+            await b_sts.edit(f"Users broadcast in progress...\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>", reply_markup=InlineKeyboardMarkup(btn))
+    await b_sts.edit(f"Users broadcast completed.\nCompleted in {time_taken}\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>")
 
 
 @Client.on_message(filters.command("grp_broadcast") & filters.user(ADMINS) & filters.reply)
 async def groups_broadcast(bot, message):
     chats = await db.get_all_chats()
     b_msg = message.reply_to_message
-    sts = await message.reply_text(
+    b_sts = await message.reply_text(
         text='Broadcasting your groups messages...'
     )
     start_time = time.time()
     total_chats = await db.total_chat_count()
     done = 0
-    failed =0
+    failed = 0
     temp.GROUPS_CANCEL = False
     success = 0
         
     async for chat in chats:
         time_taken = get_readable_time(time.time()-start_time)
         if temp.GROUPS_CANCEL:
-            await sts.edit(f"Groups broadcast Cancelled!\nCompleted in {time_taken}\n\nTotal Groups: <code>{total_chats}</code>\nCompleted: <code>{done} / {total_chats}</code>\nSuccess: <code>{success}</code>\nFailed: <code>{failed}</code>")
+            await b_sts.edit(f"Groups broadcast Cancelled!\nCompleted in {time_taken}\n\nTotal Groups: <code>{total_chats}</code>\nCompleted: <code>{done} / {total_chats}</code>\nSuccess: <code>{success}</code>\nFailed: <code>{failed}</code>")
             return
         sts = await groups_broadcast_messages(int(chat['id']), b_msg)
         if sts == 'Success':
@@ -79,7 +79,7 @@ async def groups_broadcast(bot, message):
             btn = [[
                 InlineKeyboardButton('CANCEL', callback_data=f'broadcast_cancel#groups')
             ]]
-            await sts.edit(f"Groups groadcast in progress...\n\nTotal Groups: <code>{total_chats}</code>\nCompleted: <code>{done} / {total_chats}</code>\nSuccess: <code>{success}</code>\nFailed: <code>{failed}</code>", reply_markup=InlineKeyboardMarkup(btn))    
-    await sts.edit(f"Groups broadcast completed.\nCompleted in {time_taken}\n\nTotal Groups: <code>{total_chats}</code>\nCompleted: <code>{done} / {total_chats}</code>\nSuccess: <code>{success}</code>\nFailed: <code>{failed}</code>")
+            await b_sts.edit(f"Groups groadcast in progress...\n\nTotal Groups: <code>{total_chats}</code>\nCompleted: <code>{done} / {total_chats}</code>\nSuccess: <code>{success}</code>\nFailed: <code>{failed}</code>", reply_markup=InlineKeyboardMarkup(btn))    
+    await b_sts.edit(f"Groups broadcast completed.\nCompleted in {time_taken}\n\nTotal Groups: <code>{total_chats}</code>\nCompleted: <code>{done} / {total_chats}</code>\nSuccess: <code>{success}</code>\nFailed: <code>{failed}</code>")
 
 
