552d69a41e17f9a37c4e865051ee2c611d8198a1
Hansaka Anuhas <141299098+HansakaAnuhas-TG@users.noreply.github.com>
2023-11-30 11:27:57 +0530

    #

--- bot.py
+++ bot.py
@@ -1,12 +1,4 @@
 import logging
-import logging.config
-
-# Get logging configurations
-logging.config.fileConfig('logging.conf')
-logging.getLogger().setLevel(logging.INFO)
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
-logging.getLogger("cinemagoer").setLevel(logging.ERROR)
-
 from pyrogram import Client, __version__
 import pyromod.listen
 from pyrogram.raw.all import layer
