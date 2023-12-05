0e66b9cb629f293cbf9b7894d8321a4651d99048
Hansaka Anuhas <141299098+HansakaAnuhas-TG@users.noreply.github.com>
2023-12-05 16:45:54 +0530

    -

--- utils.py
+++ utils.py
@@ -262,23 +262,3 @@ def get_wish():
     else:
         status = "ɢᴏᴏᴅ ᴇᴠᴇɴɪɴɢ 🌘"
     return status
-
-def get_media_from_message(message: "Message") -> Any:
-    media_types = (
-        "audio",
-        "document",
-        "photo",
-        "sticker",
-        "animation",
-        "video",
-        "voice",
-        "video_note",
-    )
-    for attr in media_types:
-        media = getattr(message, attr, None)
-        if media:
-            return media
-
-def get_hash(media_msg: Message) -> str:
-    media = get_media_from_message(media_msg)
-    return getattr(media, "file_unique_id", "")[:6]
