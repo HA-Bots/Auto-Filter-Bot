a9b355859586bccbf0a862e268318827c27c9175
Hansaka Anuhas <141299098+HansakaAnuhas-TG@users.noreply.github.com>
2023-12-05 18:44:44 +0530

    -

--- plugins/commands.py
+++ plugins/commands.py
@@ -18,7 +18,6 @@ import sys
 from shortzy import Shortzy
 from telegraph import upload_file
 
-logger = logging.getLogger(__name__)
 
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
