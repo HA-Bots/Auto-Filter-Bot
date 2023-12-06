a8fee871683cc5954eca989ab0d8c708d67bc6dd
Hansaka Anuhas <141299098+HansakaAnuhas-TG@users.noreply.github.com>
2023-12-06 10:41:01 +0530

    -

--- plugins/openai.py
+++ plugins/openai.py
@@ -1,9 +1,9 @@
 from info import SUPPORT_GROUP, SUPPORT_LINK, OPENAI_API
 from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-import openai
+from openai import OpenAI
 
-openai.api_key = OPENAI_API
+ai_client = OpenAI(api_key=OPENAI_API)
 
 @Client.on_message(filters.command("openai"))
 async def ask_question(client, message):
@@ -20,11 +20,12 @@ async def ask_question(client, message):
         return await message.reply_text("Command Incomplete!\nUsage: /openai your_question")
     msg = await message.reply("Searching...")
     try:
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=text,
-            max_tokens=2000
+        response = ai_client.chat.completions.create(
+            messages=[
+                {"role": "user", "content": text}
+            ],
+            model="gpt-3.5-turbo"
         )
-        await msg.edit(f"User: {message.from_user.mention}\nQuery: <code>{text}</code>\n\nResults:\n\n<code>{response.choices[0].text}</code>")
+        await msg.edit(f"User: {message.from_user.mention}\nQuery: <code>{text}</code>\n\nResults:\n\n<code>{response}</code>")
     except Exception as e:
         await msg.edit(f'Error - <code>{e}</code>')
