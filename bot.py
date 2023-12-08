f49b50dd9bc9e38e3fcd557c178d2a8bdf8cb254
Hansaka Anuhas <141299098+HansakaAnuhas-TG@users.noreply.github.com>
2023-12-08 09:53:06 +0530

    typo

--- bot.py
+++ bot.py
@@ -1,7 +1,5 @@
-import logging
-from pyrogram import Client, __version__
 import pyromod
-from pyrogram.raw.all import layer
+from pyrogram import Client, __version__
 from database.ia_filterdb import Media
 from aiohttp import web
 from database.users_chats_db import db
@@ -10,7 +8,7 @@ from info import LOG_CHANNEL, API_ID, API_HASH, BOT_TOKEN, PORT, BIN_CHANNEL
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
-import time, os
+import time, os, platform
 from pyrogram.errors import AccessTokenExpired, AccessTokenInvalid
 
 
@@ -48,7 +46,7 @@ class Bot(Client):
         app = web.AppRunner(await web_server())
         await app.setup()
         await web.TCPSite(app, "0.0.0.0", PORT).start()
-        print(f"\nBot [{username}] Started!\n")
+        print(f"\nPyrogram [{__version__}] Bot [{username}] Started With Python [{platform.python_version()}]\n")
         try:
             await self.send_message(chat_id=LOG_CHANNEL, text=f"<b>{me.mention} Restarted! 🤖</b>")
         except:
@@ -66,6 +64,40 @@ class Bot(Client):
         await super().stop()
         print("Bot Stopped! Bye...")
 
+
+    async def iter_messages(self: Client, chat_id: Union[int, str], limit: int, offset: int = 0) -> Optional[AsyncGenerator["types.Message", None]]:
+        """Iterate through a chat sequentially.
+        This convenience method does the same as repeatedly calling :meth:`~pyrogram.Client.get_messages` in a loop, thus saving
+        you from the hassle of setting up boilerplate code. It is useful for getting the whole chat messages with a
+        single call.
+        Parameters:
+            chat_id (``int`` | ``str``):
+                Unique identifier (int) or username (str) of the target chat.
+                For your personal cloud (Saved Messages) you can simply use "me" or "self".
+                For a contact that exists in your Telegram address book you can use his phone number (str).
+                
+            limit (``int``):
+                Identifier of the last message to be returned.
+                
+            offset (``int``, *optional*):
+                Identifier of the first message to be returned.
+                Defaults to 0.
+        Returns:
+            ``Generator``: A generator yielding :obj:`~pyrogram.types.Message` objects.
+        Example:
+            .. code-block:: python
+                async for message in app.iter_messages("pyrogram", 1000, 100):
+                    print(message.text)
+        """
+        current = offset
+        while True:
+            new_diff = min(200, limit - current)
+            if new_diff <= 0:
+                return
+            messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
+            for message in messages:
+                yield message
+                current += 1
 
 app = Bot()
 app.run()
