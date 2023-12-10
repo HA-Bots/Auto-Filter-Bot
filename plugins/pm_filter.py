ca3ddf0ff50563ef86e0019d74f8636f0ea882a2
Bikash Maity <147458715+CoderGrill@users.noreply.github.com>
2023-12-10 08:54:09 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -13,7 +13,7 @@ from pyrogram import Client, filters, enums
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid, ChatAdminRequired
 from utils import get_size, is_subscribed, is_check_admin, get_wish, get_shortlink, get_verify_status, update_verify_status, get_readable_time, get_poster, temp, get_settings, save_group_settings
 from database.users_chats_db import db
-from database.ia_filterdb import Media, get_file_details, get_search_results,delete_files
+from database.ia_filterdb import Media, get_file_details, get_search_results, delete_files
 import logging
 
 BUTTONS = {}
