be16e263addbcbb453e60a8b6472be5bc27e16f4
Hansaka Anuhas <141299098+HansakaAnuhas-TG@users.noreply.github.com>
2023-12-17 15:05:52 +0530

    as pin msg

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -19,10 +19,14 @@ async def broadcast_cancel(bot, query):
         temp.GROUPS_CANCEL = True
         await query.message.edit("Trying to cancel groups broadcasting...")
                
-@Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
+@Client.on_message(filters.command(["broadcast", "pin_broadcast"]) & filters.user(ADMINS) & filters.reply)
 async def users_broadcast(bot, message):
     if lock.locked():
         return await message.reply('Currently broadcast processing, Wait for complete.')
+    if message.command[0] == 'pin_broadcast':
+        pin = True
+    else:
+        pin = False
     users = await db.get_all_users()
     b_msg = message.reply_to_message
     b_sts = await message.reply_text(text='Broadcasting your users messages...')
@@ -39,7 +43,7 @@ async def users_broadcast(bot, message):
                 temp.USERS_CANCEL = False
                 await b_sts.edit(f"Users broadcast Cancelled!\nCompleted in {time_taken}\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>")
                 return
-            sts = await broadcast_messages(int(user['id']), b_msg)
+            sts = await broadcast_messages(int(user['id']), b_msg, pin)
             if sts == 'Success':
                 success += 1
             elif sts == 'Error':
@@ -53,10 +57,14 @@ async def users_broadcast(bot, message):
         await b_sts.edit(f"Users broadcast completed.\nCompleted in {time_taken}\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>")
 
 
-@Client.on_message(filters.command("grp_broadcast") & filters.user(ADMINS) & filters.reply)
+@Client.on_message(filters.command(["grp_broadcast", "pin_grp_broadcast"]) & filters.user(ADMINS) & filters.reply)
 async def groups_broadcast(bot, message):
     if lock.locked():
         return await message.reply('Currently broadcast processing, Wait for complete.')
+    if message.command[0] == 'pin_grp_broadcast':
+        pin = True
+    else:
+        pin = False
     chats = await db.get_all_chats()
     b_msg = message.reply_to_message
     b_sts = await message.reply_text(text='Broadcasting your groups messages...')
@@ -73,7 +81,7 @@ async def groups_broadcast(bot, message):
                 temp.GROUPS_CANCEL = False
                 await b_sts.edit(f"Groups broadcast Cancelled!\nCompleted in {time_taken}\n\nTotal Groups: <code>{total_chats}</code>\nCompleted: <code>{done} / {total_chats}</code>\nSuccess: <code>{success}</code>\nFailed: <code>{failed}</code>")
                 return
-            sts = await groups_broadcast_messages(int(chat['id']), b_msg)
+            sts = await groups_broadcast_messages(int(chat['id']), b_msg, pin)
             if sts == 'Success':
                 success += 1
             elif sts == 'Error':
