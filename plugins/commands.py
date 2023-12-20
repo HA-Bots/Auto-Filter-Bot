cf592c9c8cc2a66a328dffa5453f77e27eeb6c61
Hansaka Anuhas <141299098+HansakaAnuhas-TG@users.noreply.github.com>
2023-12-20 10:32:41 +0530

    fixes

--- plugins/commands.py
+++ plugins/commands.py
@@ -202,9 +202,10 @@ async def stats(bot, message):
     files = await Media.count_documents()
     users = await db.total_users_count()
     chats = await db.total_chat_count()
-    size = get_size(await db.get_db_size())
+    u_size = get_size(await db.get_db_size())
+    f_size = get_size(536870912 - await db.get_db_size())
     uptime = get_readable_time(time.time() - temp.START_TIME)
-    await msg.edit(script.STATUS_TXT.format(files, users, chats, size, uptime))    
+    await msg.edit(script.STATUS_TXT.format(files, users, chats, u_size, f_size, uptime))    
     
 @Client.on_message(filters.command('settings'))
 async def settings(client, message):
