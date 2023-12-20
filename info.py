f8b91a8f879662f187a8689870b8d5d81ec47959
Dhirajbhawani <153968088+Dhirajbhawani@users.noreply.github.com>
2023-12-20 20:43:13 +0545

    Update info.py

--- info.py
+++ info.py
@@ -114,7 +114,7 @@ if len(BIN_CHANNEL) == 0:
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "https://t.me/animal_movie720p")
+URL = environ.get("URL", "https://t.me/DB_film_store")
 if len(URL) == 0:
     print('Error - URL is missing, exiting now')
     exit()
