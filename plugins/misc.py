f5fcbd73a74736ce0195102552de728f689fc5ec
Hansaka Anuhas <141299098+HansakaAnuhas-TG@users.noreply.github.com>
2023-12-28 09:27:28 +0530

    added speedtest checker

--- plugins/misc.py
+++ plugins/misc.py
@@ -1,5 +1,7 @@
 import os
+from speedtest import Speedtest, ConfigRetrievalError
 from pyrogram import Client, filters, enums
+from utils import get_size
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 import logging
 
@@ -15,3 +17,47 @@ async def showid(client, message):
 
     elif chat_type == enums.ChatType.CHANNEL:
         await message.reply_text(f'★ Channel ID: <code>{message.chat.id}</code>')
+
+
+@Client.on_message(filters.command('speedtest'))
+async def speedtest(client, message):
+    #from - https://github.com/weebzone/WZML-X/blob/master/bot/modules/speedtest.py
+    msg = await message.reply_text("Initiating Speedtest...")
+    try:
+        speed = Speedtest()
+    except ConfigRetrievalError:
+        await msg.edit("Can't connect to Server at the Moment, Try Again Later !")
+        return
+    speed.get_best_server()
+    speed.download()
+    speed.upload()
+    speed.results.share()
+    result = speed.results.dict()
+    photo = result['share']
+    text = f'''
+➲ <b>SPEEDTEST INFO</b>
+┠ <b>Upload:</b> <code>{get_size(result['upload'] / 8)}/s</code>
+┠ <b>Download:</b>  <code>{get_size(result['download'] / 8)}/s</code>
+┠ <b>Ping:</b> <code>{result['ping']} ms</code>
+┠ <b>Time:</b> <code>{result['timestamp']}</code>
+┠ <b>Data Sent:</b> <code>{get_size(int(result['bytes_sent']))}</code>
+┖ <b>Data Received:</b> <code>{get_size(int(result['bytes_received']))}</code>
+
+➲ <b>SPEEDTEST SERVER<</b>
+┠ <b>Name:</b> <code>{result['server']['name']}</code>
+┠ <b>Country:</b> <code>{result['server']['country']}, {result['server']['cc']}</code>
+┠ <b>Sponsor:</b> <code>{result['server']['sponsor']}</code>
+┠ <b>Latency:</b> <code>{result['server']['latency']}</code>
+┠ <b>Latitude:</b> <code>{result['server']['lat']}</code>
+┖ <b>Longitude:</b> <code>{result['server']['lon']}</code>
+
+➲ <b>CLIENT DETAILS</b>
+┠ <b>IP Address:</b> <code>{result['client']['ip']}</code>
+┠ <b>Latitude:</b> <code>{result['client']['lat']}</code>
+┠ <b>Longitude:</b> <code>{result['client']['lon']}</code>
+┠ <b>Country:</b> <code>{result['client']['country']}</code>
+┠ <b>ISP:</b> <code>{result['client']['isp']}</code>
+┖ <b>ISP Rating:</b> <code>{result['client']['isprating']}</code>
+'''
+    await message.reply_photo(photo=photo, caption=text)
+    await msg.delete()
