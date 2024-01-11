fdbdd8c0f51cc5967f8b72f674d49218a4f5a630
technical_aks <105633032+technicalaks@users.noreply.github.com>
2024-01-11 17:46:06 +0530

    Done

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -1,5 +1,5 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-from info import DATABASE_NAME, DATABASE_URL, IMDB_TEMPLATE, WELCOME_TEXT, AUTH_CHANNEL, LINK_MODE, TUTORIAL, SHORTLINK_URL, SHORTLINK_API, SHORTLINK, FILE_CAPTION, IMDB, WELCOME, SPELL_CHECK, PROTECT_CONTENT, AUTO_FILTER, AUTO_DELETE, STREAM_TORF
+from info import DATABASE_NAME, DATABASE_URL, IMDB_TEMPLATE, WELCOME_TEXT, AUTH_CHANNEL, LINK_MODE, TUTORIAL, SHORTLINK_URL, SHORTLINK_API, SHORTLINK, FILE_CAPTION, IMDB, WELCOME, SPELL_CHECK, PROTECT_CONTENT, AUTO_FILTER, AUTO_DELETE, IS_STREAM
 
 client = AsyncIOMotorClient(DATABASE_URL)
 mydb = client[DATABASE_NAME]
@@ -35,7 +35,6 @@ class Database:
         self.grp = mydb.Groups
         self.me = mydb.Me
 
-
     def new_user(self, id, name):
         return dict(
             id = id,
@@ -46,7 +45,6 @@ class Database:
             ),
             verify_status=self.default_verify
         )
-
 
     def new_group(self, id, title):
         return dict(
@@ -98,13 +96,11 @@ class Database:
     async def get_all_users(self):
         return self.col.find({})
     
-
     async def delete_user(self, user_id):
         await self.col.delete_many({'id': int(user_id)})
 
     async def delete_chat(self, grp_id):
         await self.grp.delete_many({'id': int(grp_id)})
-
 
     async def get_banned(self):
         users = self.col.find({'ban_status.is_banned': True})
@@ -113,17 +109,14 @@ class Database:
         b_users = [user['id'] async for user in users]
         return b_users, b_chats
     
-
     async def add_chat(self, chat, title):
         chat = self.new_group(chat, title)
         await self.grp.insert_one(chat)
-    
 
     async def get_chat(self, chat):
         chat = await self.grp.find_one({'id':int(chat)})
         return False if not chat else chat.get('chat_status')
     
-
     async def re_enable_chat(self, id):
         chat_status=dict(
             is_disabled=False,
@@ -132,8 +125,7 @@ class Database:
         await self.grp.update_one({'id': int(id)}, {'$set': {'chat_status': chat_status}})
         
     async def update_settings(self, id, settings):
-        await self.grp.update_one({'id': int(id)}, {'$set': {'settings': settings}})
-        
+        await self.grp.update_one({'id': int(id)}, {'$set': {'settings': settings}})      
     
     async def get_settings(self, id):
         chat = await self.grp.find_one({'id':int(id)})
@@ -141,7 +133,6 @@ class Database:
             return chat.get('settings', self.default_setgs)
         return self.default_setgs
     
-
     async def disable_chat(self, chat, reason="No Reason"):
         chat_status=dict(
             is_disabled=True,
@@ -149,45 +140,37 @@ class Database:
             )
         await self.grp.update_one({'id': int(chat)}, {'$set': {'chat_status': chat_status}})
     
-
     async def get_verify_status(self, user_id):
         user = await self.col.find_one({'id':int(user_id)})
         if user:
             return user.get('verify_status', self.default_verify)
         return self.default_verify
         
-
     async def update_verify_status(self, user_id, verify):
         await self.col.update_one({'id': int(user_id)}, {'$set': {'verify_status': verify}})
-
     
     async def total_chat_count(self):
         count = await self.grp.count_documents({})
         return count
     
-
     async def get_all_chats(self):
         return self.grp.find({})
-
 
     async def get_db_size(self):
         return (await mydb.command("dbstats"))['dataSize']
 
-
-    async def get_stream_info(self, botid):
-        user = await self.me.find_one({'id':int(botidid)})
+    async def get_stream_info(self, bot_id):
+        user = await self.me.find_one({'id':int(bot_id)})
         if user:
-            return user.get('stream_torf', True)
+            return user.get('is_stream', True)
         else:
-            return STREAM_TORF
+            return IS_STREAM
 
-
-    async def update_stream_info(self, botid, value):
-        user = await self.me.find_one({'id':int(botid)})
+    async def update_stream_info(self, bot_id, value):
+        user = await self.me.find_one({'id':int(bot_id)})
         if user:
-            await self.me.update_one({'id': int(botid)}, {'$set': {'stream_torf': value}})
+            await self.me.update_one({'id': int(bot_id)}, {'$set': {'is_stream': value}})
         else:
-            await self.me.insert_one({'id': int(botid)}, {'$set': {'stream_torf': value}})
+            await self.me.insert_one({'id': int(bot_id)}, {'$set': {'is_stream': value}})
         
-
 db = Database()
