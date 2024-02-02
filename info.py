ec7a261f055ed3e1ea5fd6700599b1f01639f201
vpcreatz <119391455+vpcreatz@users.noreply.github.com>
2024-02-02 20:07:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -112,6 +112,8 @@ IMDB = is_enabled('IMDB', True)
 SPELL_CHECK = is_enabled("SPELL_CHECK", True)
 SHORTLINK = is_enabled('SHORTLINK', False)
 
+PAYMENT_QR = environ.get('PAYMENT_QR', 'http://graph.org/file/cacbbea472e5a48ce0d64.jpg')
+
 # for stream
 IS_STREAM = is_enabled('IS_STREAM', True)
 BIN_CHANNEL = environ.get("BIN_CHANNEL", "")
