3d973010589f8b53e26979cb6d750c348174810b
technical_aks <105633032+technicalaks@users.noreply.github.com>
2024-02-04 11:40:58 +0530

    Done

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -203,5 +203,12 @@ class Database:
         expiry_time = datetime.datetime.now() + datetime.timedelta(seconds=seconds)
         user_data = {"id": user_id, "expiry_time": expiry_time, "has_free_trial": True}
         await self.users.update_one({"id": user_id}, {"$set": user_data}, upsert=True)
-        
+
+    async def all_premium_users(self):
+        count = await self.users.count_documents({
+            "expiry_time": {"$gt": datetime.datetime.now()},
+            "has_free_trial": {"$ne": True}
+        })
+        return count
+
 db = Database()
