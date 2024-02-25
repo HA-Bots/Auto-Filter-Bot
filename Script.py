edb72718ef3a6ad95d810bb700ed7f53786409d4
vpcreatz <119391455+vpcreatz@users.noreply.github.com>
2024-02-25 17:04:47 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -139,3 +139,5 @@ I can't find the <b>{}</b> in my database! 🥲
 - ꜱᴏᴜʀᴄᴇ - <a href=https://github.com/HA-Bots/Auto-Filter-Bot>ʜᴇʀᴇ</a>
 
 - ᴅᴇᴠʟᴏᴘᴇʀ - @HA_Bots"""
+
+   SPAM_TXT = """{} Don't Spam, Wait For {}"""
