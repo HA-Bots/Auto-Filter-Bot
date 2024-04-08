e0fc476f31181696217f9e287df84b89d4f2b8c8
KD Botz <116437604+KDBotz@users.noreply.github.com>
2024-04-08 19:49:03 +0530

    query

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -161,7 +161,7 @@ async def next_page(bot, query):
         ]
             for file in files
         ]
-    if settings['shortlink'] and not await db.has_premium_access(message.from_user.id):
+    if settings['shortlink'] and not await db.has_premium_access(query.message.from_user.id):
         btn.insert(0,
             [InlineKeyboardButton("♻️ sᴇɴᴅ ᴀʟʟ ♻️", url=await get_shortlink(settings['url'], settings['api'], f'https://t.me/{temp.U_NAME}?start=all_{query.message.chat.id}_{key}')),
             InlineKeyboardButton("🥇 ʙᴜʏ 🥇", url=f"https://t.me/{temp.U_NAME}?start=plans"),
@@ -313,7 +313,7 @@ async def lang_next_page(bot, query):
         ]
             for file in files
         ]
-    if settings['shortlink'] and not await db.has_premium_access(message.from_user.id):
+    if settings['shortlink'] and not await db.has_premium_access(query.message.from_user.id):
         btn.insert(0,
             [InlineKeyboardButton("♻️ sᴇɴᴅ ᴀʟʟ ♻️", url=await get_shortlink(settings['url'], settings['api'], f'https://t.me/{temp.U_NAME}?start=all_{query.message.chat.id}_{key}')),
             InlineKeyboardButton("🥇 ʙᴜʏ 🥇", url=f"https://t.me/{temp.U_NAME}?start=plans")]
