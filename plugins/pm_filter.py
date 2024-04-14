ddaf0ab7c6a428ea0edaeaf564a87def29b8e9d4
vpcreatz <119391455+vpcreatz@users.noreply.github.com>
2024-04-14 23:35:27 +0530

    Typo

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -30,7 +30,7 @@ async def give_filter(client, message):
     chatid = message.chat.id
     userid = message.from_user.id if message.from_user else None
     user_id = message.from_user.id if message.from_user else 0
-    fsub = settings['fsub'] if not await db.has_premium_access(message.from_user.id)
+    fsub = settings['fsub'] if not await db.has_premium_access(message.from_user.id) else None
     if settings.get('is_fsub', IS_FSUB) and fsub is not None:
         try:
             btn = await is_subscribed(client, message, int(fsub))
