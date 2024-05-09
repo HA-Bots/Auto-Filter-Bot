195192cfa22de16c13b605abc1c89ef326c02ccf
technical_aks <105633032+technicalaks@users.noreply.github.com>
2024-05-09 15:48:56 +0530

    Done.

--- info.py
+++ info.py
@@ -103,6 +103,7 @@ INDEX_EXTENSIONS = [extensions.lower() for extensions in environ.get('INDEX_EXTE
 PM_DELETE_TIME = int(environ.get('PM_DELETE_TIME', 600))
 
 # boolean settings
+IS_PM_SEARCH = is_enabled('IS_PM_SEARCH', False)
 IS_VERIFY = is_enabled('IS_VERIFY', False)
 AUTO_DELETE = is_enabled('AUTO_DELETE', False)
 WELCOME = is_enabled('WELCOME', False)
