233880a73c0ee8028921cd57fd85279e64b7c793
vpcreatz <119391455+vpcreatz@users.noreply.github.com>
2024-05-10 17:07:41 +0530

    .
    Fixed `TypeError: int() argument must be a string, a bytes-like object or a real number, not 'list'`

--- utils.py
+++ utils.py
@@ -35,8 +35,8 @@ async def is_subscribed(bot, query, channel):
     btn = []
     if AUTH_CHANNEL:
         channel.append(AUTH_CHANNEL)
-    print(channel)
-    for id in channel:
+    for chat_id in channel:
+        id = chat_id[0]
         chat = await bot.get_chat(int(id))
         try:
             await bot.get_chat_member(id, query.from_user.id)
