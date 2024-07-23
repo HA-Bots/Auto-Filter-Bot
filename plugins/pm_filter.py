a4a897bd70cfb1a3806425c69c0b6185a4523998
vpcreatz <119391455+vpcreatz@users.noreply.github.com>
2024-07-23 20:36:47 +0530

    Typo

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1050,7 +1050,7 @@ async def auto_filter(client, msg, s, spoll=False):
                 except:
                     pass
     else:
-        k = await s.edit_text(cap + files_link + del_msg, reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=True, parse_mode=enums.ParseMode.HTML, quote=True)
+        k = await s.edit_text(cap + files_link + del_msg, reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=True, parse_mode=enums.ParseMode.HTML)
         if settings["auto_delete"]:
             await asyncio.sleep(DELETE_TIME)
             await k.delete()
