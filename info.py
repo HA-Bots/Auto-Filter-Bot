6becf232bef083788a8fad1d403f298d19df3bc1
All-Movies-here <sumantsharma119@gmail.com>
2024-09-17 18:22:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ def is_valid_ip(ip):
     return re.match(ip_pattern, ip) is not None
 
 # Bot information
-API_ID = environ.get('API_ID', '')
+API_ID = environ.get('API_ID', '28735699')
 if len(API_ID) == 0:
     print('Error - API_ID is missing, exiting now')
     exit()
